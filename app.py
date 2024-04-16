4566deef89f6f62e66048843aaea73638afe6eae
Yagiz Ulasoglu <yulasoglu@gmail.com>
2024-04-16 14:50:48 -0700

    Worked on aws

--- app.py
+++ app.py
@@ -3,6 +3,9 @@
 import os
 from dotenv import load_dotenv
 
+import boto3
+
+
 from flask import Flask, render_template, flash, redirect, session, g, jsonify, request
 # # from flask_debugtoolbar import DebugToolbarExtension
 # from sqlalchemy.exc import IntegrityError
@@ -18,6 +21,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", 'postgresql:///sharebb')
 app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
+app.config['AWS_SECRET_KEY'] = os.environ.get('AWS_SECRET_KEY')
+app.config['ACCESS_KEY'] = os.environ.get('ACCESS_KEY')
 
 if app.debug:
     app.config['SQLALCHEMY_ECHO'] = True
@@ -27,3 +32,39 @@ if app.debug:
 # toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
+
+s3 = boto3.client(
+    "s3",
+    "us-east-1",
+    aws_access_key_id=os.environ.get('ACCESS_KEY'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'),
+)
+
+
+print((s3.list_buckets()).Buckets, "#####################################")
+# for bucket in s3.buckets.all():
+#     print(bucket.name)
+
+with open('test.jpeg', 'rb') as data:
+    s3.put_object(s3.list_buckets().Buckets[0].Name).put_object(
+        Key='test.jpeg', Body=data)
+
+# try:
+#     response = s3_client.put_object(
+#         Bucket=s3.Buckets.list_buckets[0].Name,
+#         Key=object_key
+#     )
+# except:
+#     print("error")
+
+
+# response = s3_client.put_object(
+#     Bucket='your-bucket-name',  # Name of the S3 bucket
+#     Key='path/to/your/object/file.txt',  # Key (path) where the object will be stored within the bucket
+#     Body=b'Object content',  # Data (content) of the object to be uploaded
+#     # ContentType='text/plain',  # Optional: Content type of the object (MIME type)
+#     # ACL='private',  # Optional: Access Control List for the object (e.g., 'private', 'public-read')
+#     # Metadata={'key': 'value'},  # Optional: Metadata associated with the object
+#     # StorageClass='STANDARD',  # Optional: Storage class of the object (e.g., 'STANDARD', 'STANDARD_IA')
+#     # ServerSideEncryption='AES256'  # Optional: Server-side encryption algorithm (e.g., 'AES256')
+# )
